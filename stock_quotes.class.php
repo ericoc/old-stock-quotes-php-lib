3959d20117e3fa6522d189f45b6ac28ce53a9d8b
Eric OC <eric@ericoc.com>
2017-09-10 11:44:45 -0400

    fix UA

--- stock_quotes.class.php
+++ stock_quotes.class.php
@@ -13,7 +13,7 @@ class stock_quotes {
 
 	// Set timeout (for Redis and cURL) as well as user agent for HTTP(S) requests to Yahoo Finance SQL API
 	private $timeout = 2;
-	private $user_agent = 'EricOC Stock Class - https://github.com/ericoc/hotbot/blob/master/stock_quotes.class.php';
+	private $user_agent = 'EricOC Stock Quote PHP Library / https://github.com/ericoc/stock-quotes-php-lib/';
 
 	// Create constructor function to connect to Redis
 	function __construct () {
